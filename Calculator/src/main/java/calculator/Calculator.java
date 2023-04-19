e4d97db2ad7e4032b412c780f252d2133a29dad4
Marko Mijatović <mijat5555@gmail.com>
2023-04-19 13:58:45 +0200

    Kreiranje Kalkulatora

---
+++ Calculator/src/main/java/calculator/Calculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package calculator;
+
+import java.awt.SplashScreen;
+
+/**
+ *
+ * @author Marko
+ */
+public class Calculator {
+public static void main(String[] args) {
+   new CalculatorApp().setVisible(true);
+}
+}
